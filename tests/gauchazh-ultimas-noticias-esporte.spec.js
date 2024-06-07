b33ea0541ff6ec3ce580b38ce183d4fa47f2ee3c
Henrique Tres Terra <116606420+Henrique-Terra@users.noreply.github.com>
2024-06-07 17:06:43 -0300

    .

--- tests/gauchazh-ultimas-noticias-esporte.spec.js
+++ tests/gauchazh-ultimas-noticias-esporte.spec.js
@@ -1,13 +1,11 @@
 const { test } = require('@playwright/test');
 
 test('Verificar últimas notícias de esporte na GauchaZH', async ({ page }) => {
-  // Dado que acesso a seção de últimas notícias de esporte
+
   await page.goto('https://gaucha.clicrbs.com.br/esporte/');
 
-  // Então verifico se as manchetes das notícias são relevantes e atualizadas
   const manchetes = await page.locator('.noticia__titulo');
 
-  // Verifica se "manchetes" é um array
   if (Array.isArray(manchetes)) {
     for (const manchete of manchetes) {
       const textoManchete = await manchete.textContent();
